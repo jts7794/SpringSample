7667babfeb197a97a4c7c760c6b90cbea339abce
jts20 <jts20@DESKTOP-KC2643L>
2020-04-13 17:29:22 +0900

    포스트방식으로 화면(hello.html)로부터 다른 화면(helloResponse.html)까지 값 이동

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +15,23 @@ public class HelloController {
 		// hello.html이동
 		return "hello";
 	}
+	
+
+	// Point1 -> @PostMapping : @GetMapping랑 같은 양식, 메소드에@PostMapping어노테이션을 붙인다. Post메소드로 보내진 경우 처리가 가능하도록 
+	// Point2 -> @RequestParam : 메소드 인수에 @RequestParam어노테이션을 붙이면, 화면에서부터 입력 내용을 받는다. 어노테이션 인수는 html의 name속성의 값을 지정한다.
+	//                           hello.html의 소스 (일부 발췌) : <input type="text" name="text1" th:value="${text1_value}"/>
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1")String str, 
+			Model model) {
+		
+		// Point3 -> model.addAttribute : 여기에 Key랑 값을 설정하면 화면(html)로 부터 지정한 Key의 값을 받을수 있다. 
+		// 화면에서 받은 문자열을 Model객체에 등록
+		model.addAttribute("sample", str);
+		
+		// helloResponse.html에 이동
+		return "helloResponse";
+		
+	}
+	
 }
